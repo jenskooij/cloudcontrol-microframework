d00084a70cb108232b8a9f8c766c594d57ac7ed4
Jens Kooij <jenskooij1988@gmail.com>
2019-02-14 16:34:59 +0100

    Implemented method for retrieving the request method

--- src/Request.php
+++ src/Request.php
@@ -19,6 +19,13 @@ class Request
     const SERVER_ATTR_SERVER_PORT = 'SERVER_PORT';
     const SERVER_ATTR_HTTP_X_FORWARDED_PROTO = 'HTTP_X_FORWARDED_PROTO';
     const SERVER_ATTR_REMOTE_ADDR = 'REMOTE_ADDR';
+    const METHOD_GET = 'GET';
+    const METHOD_POST = 'POST';
+    const METHOD_PUT = 'PUT';
+    const METHOD_DELETE = 'DELETE';
+    const METHOD_HEAD = 'HEAD';
+    const METHOD_OPTIONS = 'OPTIONS';
+    const METHOD_OTHER = 'OTHER';
     protected static $requestUri;
     protected static $queryString;
     protected static $relativeUri;
@@ -94,4 +101,65 @@ class Request
         }
         return self::$relativeUri;
     }
+
+    /**
+     * @return array|false
+     */
+    public static function getHeaders()
+    {
+        if (function_exists('apache_request_headers')) {
+            /** @noinspection PhpComposerExtensionStubsInspection */
+            return apache_request_headers();
+
+        } else {
+            return self::getHeadersPolyfill();
+        }
+    }
+
+    /**
+     * Polyfill for when apache request headers are not set, for example
+     * when not running with apache :P
+     *
+     * @return array
+     */
+    private static function getHeadersPolyfill()
+    {
+        $arh = array();
+        $rx_http = '/\AHTTP_/';
+        foreach ($_SERVER as $key => $val) {
+            if (preg_match($rx_http, $key)) {
+                $arh_key = preg_replace($rx_http, '', $key);
+                $rx_matches = array();
+                // do some nasty string manipulations to restore the original letter case
+                // this should work in most cases
+                $rx_matches = explode('_', $arh_key);
+                if (count($rx_matches) > 0 and strlen($arh_key) > 2) {
+                    foreach ($rx_matches as $ak_key => $ak_val) {
+                        $rx_matches[$ak_key] = ucfirst($ak_val);
+                    }
+                    $arh_key = implode('-', $rx_matches);
+                }
+                $arh[$arh_key] = $val;
+            }
+        }
+        return ($arh);
+    }
+
+    public static function getRequestMethod()
+    {
+        $request_method = strtoupper(getenv('REQUEST_METHOD'));
+        $http_methods = array(
+            self::METHOD_GET,
+            self::METHOD_POST,
+            self::METHOD_PUT,
+            self::METHOD_DELETE,
+            self::METHOD_HEAD,
+            self::METHOD_OPTIONS
+        );
+        if (!in_array($request_method, $http_methods)) {
+            return self::METHOD_OTHER;
+        }
+        return $request_method;
+    }
+
 }
